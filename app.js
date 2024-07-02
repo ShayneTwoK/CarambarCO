c27db0a668382564a3c7b031ca8c2b4b6c1e09eb
Shaïnee <shaynetwok@gmail.com>
2024-07-02 19:24:41 +0200

    WIP CORS 6

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
   // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type');
 
   // Set to true if you need the website to include cookies in the requests sent
   // to the API (e.g. in case you use sessions)
@@ -29,7 +29,7 @@ app.use(function (req, res, next) {
   next();
 });
 
-app.use(cors(corsOptionsDelegate));
+app.use(cors());
 
 app.use(express.json());
 
