d2ed15d0ba5689f998652209f27d9fc65fdaf070
Shaïnee <shaynetwok@gmail.com>
2024-07-03 16:08:41 +0200

    CORS refactoring

--- app.js
+++ app.js
@@ -5,31 +5,6 @@ const blagueRoute = require('./routes/blagueRoute');
 const db = require('./models');
 const { swaggerUi, specs } = require('./config/swagger');
 
-
-// Add headers before the routes are defined
-// app.use(function (req, res, next) {
-
-//   // Website you wish to allow to connect
-//   res.setHeader('Access-Control-Allow-Origin', 'https://shaynetwok.github.io');
-
-//   // Request methods you wish to allow
-//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-//   // Request headers you wish to allow
-//   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type');
-
-//   // Set to true if you need the website to include cookies in the requests sent
-//   // to the API (e.g. in case you use sessions)
-//   res.setHeader('Access-Control-Allow-Credentials', true);
-
-//   // Pass to next layer of middleware
-//   next();
-// });
-
-// app.use(cors());
-
-// WIP : Alternative CORS plus simple à experimenté
-// Supprimer la ligne 10 à 29
 app.use(cors({
   origin: 'https://shaynetwok.github.io'
 }));
